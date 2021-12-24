62d7f8df487a5ed3e00e88df2a6fd10a9f253446
mukhlif <asaadmohamad39@gmail.com>
2021-12-24 20:37:05 +0200

    Edit

--- app.js
+++ app.js
@@ -4,35 +4,55 @@ const items=document.querySelector('ul.collection');
 loadEventListeners();
 function loadEventListeners(){
     document.querySelector('.btn').addEventListener('click',fun);
-    document.querySelector('ul.collection').addEventListener('click',removElement);
+
     document.querySelector('.clearBooks').addEventListener('click',removAll);
-    
+    document.querySelector('.table-box').addEventListener('click',removeElement);
 }
 function fun(e){
-    const inputName=document.getElementById('name').value;
-    const inputAuthor=document.getElementById('author').value;
-    const headH=document.querySelector('#headd');
-    let list=document.createElement('li');
-    const items=document.querySelector('ul.collection');
+    let inputName=document.getElementById('name');
+    let inputAuthor=document.getElementById('author');
+    const heading=document.querySelector('h3.edit');
+    // let list=document.createElement('li');
+    // const items=document.querySelector('ul.collection');
 
-if(inputName==''||inputAuthor==''){
-    headH.innerHTML='please fill the fields!';
-    headH.style.backgroundColor='red';
-    headH.style.color='white';
-    headH.style.width='75%';
-    headH.style.padding='0.5em';
+if(inputName.value==''||inputAuthor.value==''){
+    heading.innerHTML='Please fill the fields!';
+    heading.style.backgroundColor='red';
+    heading.style.color='white';
+    heading.style.width='75%';
+    heading.style.padding='0.2em';
+    
+
 }else if(inputName!=''&&inputAuthor!=''){
-    list.innerHTML=`<li class="border fw-bold p-2 mt-2 bg-dark text-light">Book Name:${inputName}<span class="ms-5" >Book Author:${inputAuthor}</span>  <a href="#" class="ms-5" id='click'><i class="fas fa-backspace"></i></a></li>`;
-    items.append(list);
+    
+    // list.innerHTML=`<li class="border fw-bold p-2 mt-2 bg-dark text-light">Book Name:${inputName}<span class="ms-5" >Book Author:${inputAuthor}</span>  <a href="#" class="ms-5" id='click'><i class="fas fa-backspace"></i></a></li>`;
+    // items.append(list);
+
+    // inputName.value='';
+    // inputAuthor='';
+    const row=document.createElement('tr');
+    row.className='border-bottom fs-5 mt-4';
+
+    row.innerHTML=`<td class=>${inputName.value}</td>
+                  <td>${inputAuthor.value}</td>
+                  <td><a href="#" class="delete mx-auto">X</a></t>`;
+
+   document.querySelector('.table-box').append(row);
+
   
 }
 e.preventDefault();
 
 }
-function removElement(e){
-    if(e.target.parentElement.classList.contains('ms-5')){
-    e.target.parentElement.parentElement.remove();
-};}
+function removeElement(e){
+    if(e.target.classList.contains('delete')){
+       e.target.parentElement.parentElement.remove();
+    }
+    else{
+        console.log(123);
+    }
+  
+}
 function removAll(e){
     items.innerHTML='';
 }
